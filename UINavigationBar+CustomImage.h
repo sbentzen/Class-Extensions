57abbefdd4e16dfb6315ad14591b574097dc5091
sbentzen <sbentzen@gmail.com>
2011-06-03 17:24:14 +0000

    adding images to the nav bar

---
+++ UINavigationBar+CustomImage.h
@@ -0,0 +1,15 @@
+//
+//  UINavigationBar+CustomImage.h
+//  iCambrian
+//
+//  Created by Shaun Bentzen on Monday, May 30 2011.
+//  Copyright 2011 Cambrian College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UINavigationBar (CustomImage)
+- (void) setBackgroundImage:(UIImage*)image;
+- (void) clearBackgroundImage;
+@end
