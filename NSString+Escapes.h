3be34c621bbea2646fb3a4017d302402cc523e85
sbentzen <sbentzen@gmail.com>
2011-06-16 14:55:43 +0000

    Fixing Stupid Class issues

---
+++ NSString+Escapes.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Escapes.h
+//  iCambrian
+//
+//  Created by Shaun Bentzen on Thursday, June 16 2011.
+//  Copyright 2011 Cambrian College. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (NSString_Escapes)
+
+- (NSString *) URLEncodedString;
+
+@end
---
+++ NSString+Escapes.m
@@ -0,0 +1,35 @@
+//
+//  NSString+Escapes.m
+//  iCambrian
+//
+//  Created by Shaun Bentzen on Thursday, June 16 2011.
+//  Copyright 2011 Cambrian College. All rights reserved.
+//
+
+#import "NSString+Escapes.h"
+
+
+@implementation NSString (NSString_Escapes)
+
+
+- (NSString *) URLEncodedString {
+    NSMutableString * output = [NSMutableString string];
+    const unsigned char * source = (const unsigned char *)[self UTF8String];
+    int sourceLen = strlen((const char *)source);
+    for (int i = 0; i < sourceLen; ++i) {
+        const unsigned char thisChar = source[i];
+        if (thisChar == ' '){
+            [output appendString:@"+"];
+        } else if (thisChar == '.' || thisChar == '-' || thisChar == '_' || thisChar == '~' || 
+                   (thisChar >= 'a' && thisChar <= 'z') ||
+                   (thisChar >= 'A' && thisChar <= 'Z') ||
+                   (thisChar >= '0' && thisChar <= '9')) {
+            [output appendFormat:@"%c", thisChar];
+        } else {
+            [output appendFormat:@"%%%02X", thisChar];
+        }
+    }
+    return output;
+}
+
+@end
